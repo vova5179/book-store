d1752954ca3924d12a01fc41b4d8c263f2a18487
vova5179 <vova5179@gmail.com>
2023-08-07 21:00:16 +0300

    added checkstyle.xml

---
+++ src/main/java/mate/academy/booksstore/BooksStoreApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.booksstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/academy/booksstore/BooksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.booksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
