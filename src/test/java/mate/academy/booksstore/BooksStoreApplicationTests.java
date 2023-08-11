aa312ca6a99c65832df6dbe13c3f99e863436c85
vova5179 <vova5179@gmail.com>
2023-08-11 14:41:04 +0300

    added test dir in source Directory

--- src/test/java/mate/academy/booksstore/BooksStoreApplicationTests.java
+++ src/test/java/mate/academy/booksstore/BooksStoreApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BooksStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
