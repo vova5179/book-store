a79d493e0377441c4a23d01d3fca2a1b4b642af0
vova5179 <vova5179@gmail.com>
2023-08-21 16:32:47 +0300

    added CustomGlobalExceptionHandler

--- src/main/java/mate/academy/booksstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/booksstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,22 @@
 package mate.academy.booksstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @Min(0)
     private BigDecimal price;
+    @Size(max = 200)
     private String description;
     private String coverImage;
 }
